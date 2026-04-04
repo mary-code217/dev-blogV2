7fec14a96b79b2bbee7f1cb6cb5d87097278450f
mary-code217 <xorl0514@naver.com>
2026-04-04 19:53:24 +0900

    refactor: 기본 템플릿 파일 제거 및 프로젝트 뼈대 정리
    Astro 기본 템플릿의 예시 파일, 폰트, 컴포넌트를 제거하고
    MaryDev 프로젝트에 맞는 최소 구조로 재정리

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'MaryDev';
+export const SITE_DESCRIPTION = '개인 기술 블로그';
