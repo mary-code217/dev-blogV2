05b84578a6066e8fee86d2320256db9980438c24
xorl0514 <xorl0514@naver.com>
2026-04-06 10:59:24 +0900

    fix: base 경로에 trailing slash 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mary-code217.github.io',
-	base: '/dev-blogV2',
+	base: '/dev-blogV2/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
